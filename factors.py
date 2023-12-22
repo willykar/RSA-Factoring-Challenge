57e2365dd50973583d6158894b9ea07bc64f62c1
willykar <wilsonnjenga799@gmail.com>
2023-12-22 10:01:55 -0800

    update

--- factors.py
+++ factors.py
@@ -3,13 +3,14 @@ import sys
 
 
 def factorize(num):
-    fact = 2
-    while (num % fact):
-        if (fact <= num):
-            fact += 1
+    """ Generate 2 factors for a given number"""
+    factor1 = 2
+    while (num % factor1):
+        if (factor1 <= num):
+            factor1 += 1
 
-    factor2 = num // fact
-    return (factor2, fact)
+    factor2 = num // factor1
+    return (factor2, factor1)
 
 
 if len(sys.argv) != 2:
@@ -22,5 +23,5 @@ lines = file.readlines()
 
 for line in lines:
     num = int(line.rstrip())
-    factor2, fact = factorize(num)
-    print(f"{num} = {factor2} * {fact}")
+    factor2, factor1 = factorize(num)
+    print(f"{num} = {factor2} * {factor1}")
