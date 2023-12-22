4f7ea2c4872f1a158dc1ce64bf73f6443db60e55
willykar <wilsonnjenga799@gmail.com>
2023-12-22 11:06:57 -0800

    update

---
+++ factor.h
@@ -0,0 +1,13 @@
+#ifndef _FACTOR_H_
+#define _FACTOR_H_
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#define _GNU_SOURCE
+
+
+int factorize(char *buffer);
+
+#endif /* _FACTOR_H_ */
